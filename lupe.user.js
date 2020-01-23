222094c654304d049b62b9f64a5d40b2496d6cb4
Usagigawara <54873357+usagiga@users.noreply.github.com>
2020-01-23 18:01:38 +0900

    Fix typo

--- lupe.user.js
+++ lupe.user.js
@@ -1,12 +1,12 @@
 // ==UserScript==
 // @name         Lupe
 // @namespace    https://github.com/usagiga/Lupe
-// @version      1.
+// @version      1.2
 // @description  Once open link of posted images on Twitter, then Lupe changes its url to original resolutions.
 // @author       Usagigawara
 // @match        https://pbs.twimg.com/media/*
 // @grant        none
-// @run-at        document-start
+// @run-at       document-start
 // ==/UserScript==
 
 (function() {
